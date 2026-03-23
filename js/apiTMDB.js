8803123de84ed5e29186d1577e7de785738f33ff
Leonardo Ribeiro <leonardo.andradegcr@gmail.com>
2026-03-23 19:27:57 -0300

    Add error handling to apiFilmes function
    Refactor apiFilmes function to include error handling.

--- js/apiTMDB.js
+++ js/apiTMDB.js
@@ -1,11 +1,15 @@
 import apiKey from "./config.js";
 
 const apiFilmes = async () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR`;
-  const responsta = await fetch(url);
-  const data = await responsta.json();
-  console.log(data);
-  return data.results.slice(0, 10);
+  try {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR`;
+    const resposta = await fetch(url);
+    const data = await resposta.json();
+    return data.results.slice(0, 10);
+  } catch (erro) {
+    console.error("Erro ao buscar filmes:", erro);
+    return [];
+  }
 };
 
 export default apiFilmes;
